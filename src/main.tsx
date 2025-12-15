5fffc82965346a783543fed511a328fdea3e3573
shiwuliya <3369906077@qq.com>
2025-12-15 10:09:32 +0800

    chore: 添加 react-scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { Provider } from "@/components/ui/provider"
+import { scan } from "react-scan";
 import { createRoot } from 'react-dom/client'
 import App from '@/app'
 import { BrowserRouter } from "react-router-dom"
 import '@/styles/tailwind.css'
 import "@/styles/font.scss"
 import "@/styles/global.scss"
+
+scan({
+  enabled: true,
+});
 
 createRoot(document.getElementById('root')!).render(
 
