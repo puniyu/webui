73adde9eabfc04a5d44451f38c68a236584e2a1b
wuliya <3369906077@qq.com>
2026-03-18 23:34:57 +0800

    ``` feat: 添加normalize.css样式重置
    引入normalize.css库来统一

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
+import 'normalize.css'
 import '@/styles/global.css'
 import { scan } from 'react-scan'
 
